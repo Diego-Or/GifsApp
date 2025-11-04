c40fc5fb08c102a781c698cbc9fdc136576cd99e
Diego Ortiz <dfortiz116@gmail.com>
2025-11-04 13:23:27 -0500

    Actualización de rutas por defecto

--- src/app/gifs/pages/trending/trending-page.ts
+++ src/app/gifs/pages/trending/trending-page.ts
@@ -1,9 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-trending',
   imports: [],
   templateUrl: './trending-page.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class Trending { }
